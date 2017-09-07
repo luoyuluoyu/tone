6bef66e7b2132579cd6d4b5893a5df9fbdb81c03
Administrator <Administrator@MS-20170324PTLC>
2017-09-07 15:12:26 +0800

    更新数据库表

--- IM/SQLScript/qq_users.sql
+++ IM/SQLScript/qq_users.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: qq
 -- ------------------------------------------------------
--- Server version	5.7.14-log
+-- Server version	5.7.16-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,52 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `counter`
---
-
-DROP TABLE IF EXISTS `counter`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `counter` (
-  `idCounter` int(11) NOT NULL AUTO_INCREMENT,
-  `Countercol` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCounter`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `detail`
---
-
-DROP TABLE IF EXISTS `detail`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `detail` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sender` varchar(45) NOT NULL,
-  `getter` varchar(45) NOT NULL,
-  `sendTime` varchar(45) NOT NULL,
-  `content` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=136 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `friends`
---
-
-DROP TABLE IF EXISTS `friends`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `friends` (
-  `id` int(11) NOT NULL,
-  `friendAccount` varchar(45) NOT NULL,
-  `ownAccount` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
@@ -76,18 +30,11 @@ CREATE TABLE `users` (
   `gender` varchar(1) DEFAULT NULL,
   `Birthday` varchar(45) DEFAULT NULL,
   `Accountnumber` int(11) DEFAULT NULL,
-  `onlineStatus` varchar(45) DEFAULT NULL,
+  `onlineStatus` varchar(45) DEFAULT '1',
+  `role` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping events for database 'qq'
---
-
---
--- Dumping routines for database 'qq'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -98,4 +45,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-01  9:51:35
+-- Dump completed on 2017-09-07 15:11:22
